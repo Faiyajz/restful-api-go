651f77f24e78b63dbbd344918fd83c51709a3610
faiyaj <faiyajz007@gmail.com>
2022-10-29 14:30:06 +0600

    all articles in json response

--- main.go
+++ main.go
@@ -1,20 +1,43 @@
 package main
 
-import(
+import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
+type Article struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Content     string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{
+			Title:       "Test Title",
+			Description: "Test Description",
+			Content:     "Test Content",
+		},
+	}
+
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Homepage Endpoint Hit")
 }
 
-func handleRequests(){
-	http.HandleFunc("/",homePage)
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
-func main(){
+func main() {
 	handleRequests()
 }
